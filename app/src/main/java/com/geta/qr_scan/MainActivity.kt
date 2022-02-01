2a86720a4c5febc442bba71bd67bca4607613cfc
Gwenael95 <gwenael.marchetti-waternaux@edu.itescia.fr>
2022-02-01 21:01:09 +0100

    init android App (gradle)

---
+++ app/src/main/java/com/geta/qr_scan/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.geta.qr_scan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
